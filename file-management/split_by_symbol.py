e4154c32c590ac54d5e9b9bff061dc3e1de29f20
andymorato <amorato@qualtrics.com>
2023-01-19 12:32:47 -0500

    Add new tool

---
+++ file-management/split_by_symbol.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Splits file by symbol onto new lines
+#
+# Usage: ./removeLeadingNums.py <filename> <split_symbol>
+
+# Imports
+import sys
+
+# Variables
+filename = sys.argv[1]
+symbol = sys.argv[2]
+
+# Read file
+file = open(filename, 'r')
+lines = file.readlines()
+
+# Replace split sybmols with newlines
+newlines = []
+for line in lines:
+    newlines += line.split(symbol)
+
+# Write lines back to the file
+file = open(filename, "w")
+for line in newlines:
+    file.write(line + "\n")
+file.close()
